bd3c4a1f523087b05ed9097a80cf1c98d080db3c
patarakaci1 <lashakhareba15@gmail.com>
2026-08-18 01:06:07 +0400

    mealList filter

---
+++ src/constants/Constants.js
@@ -0,0 +1,5 @@
+const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=sandwich";
+const INITIAL_VISIBLE = 3;
+const FILTERS = ["all", "Popular", "New", "Chef's pick"];
+
+export default { API_URL, INITIAL_VISIBLE, FILTERS };
